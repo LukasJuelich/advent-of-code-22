fc2bc0b3f81f2c2c7b087b931e70e5080b74e4d8
Lukas <lukas.juelich@bundesanzeiger.de>
2022-12-01 17:25:44 +0100

    part two done

--- day01/main.py
+++ day01/main.py
@@ -1,14 +1,25 @@
-most_calories = 0
+most_calories = [0, 0, 0]
 current_calories = 0
+
+def calcTotalOfArray(array):
+    sum = 0
+    for element in array:
+        sum += element
+    
+    return sum
 
 with open("input.txt", "r") as file:
     for line in file:
         if line != "\n":
             current_calories += int(line)
         else:
-            if current_calories > most_calories:
-                most_calories = current_calories
-
+            for index, calories in enumerate(most_calories):
+                if current_calories > calories:
+                    most_calories[index] = current_calories
+                    break
+            
+            most_calories.sort()
             current_calories = 0
 
-print(most_calories)
+    print(most_calories)
+    print(calcTotalOfArray(most_calories))
